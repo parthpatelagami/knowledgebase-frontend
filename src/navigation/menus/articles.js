e3b781d06db59eec3edf52713da2fea1b1bc3896
Aadil Mangera <aadil.mangera@agami-tech.com>
2023-12-15 17:17:23 +0530

    Meta change in Articles

--- src/navigation/menus/articles.js
+++ src/navigation/menus/articles.js
@@ -1,13 +1,11 @@
 // ** Icons Import
-import { Home } from 'react-feather'
+import { Home } from "react-feather";
 
 export default [
-    {
-        id: 'articles',
-        title: 'Articles',
-        icon: <Home size={20} />,
-        navLink: '/articles',
-        action: 'view',
-    resource: 'Articles'
-    }
-]
+  {
+    id: "articles",
+    title: "Articles",
+    icon: <Home size={20} />,
+    navLink: "/articles",
+  },
+];
