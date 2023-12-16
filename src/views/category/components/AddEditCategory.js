617667c75f4a893298ee27401edbbddbf7bd6174
Aadil Mangera <aadil.mangera@agami-tech.com>
2023-12-16 18:17:46 +0530

    Category Validations

--- src/views/category/components/AddEditCategory.js
+++ src/views/category/components/AddEditCategory.js
@@ -57,7 +57,30 @@ const AddEditCategory = ({
       name: yup
         .string()
         .required("Name is required.")
-        .min(4, "Name should be atleast 4 characters."),
+        .min(4, "Name should be atleast 4 characters.")
+        .test(
+          "Duplicate-Category",
+          "Category Name already exist",
+          async function (value) {
+            if (value) {
+              try {
+                const categoryId =
+                  type === "edit-category" ? rowInfo.category_id : null;
+                const response = await dispatch(
+                  checkCategory({ name: value, id: categoryId })
+                ).unwrap();
+                if (response.success === true) {
+                  return false;
+                } else {
+                  return true;
+                }
+              } catch (error) {
+                return false;
+              }
+            }
+            return true;
+          }
+        ),
     })
     .required();
 
@@ -96,15 +119,6 @@ const AddEditCategory = ({
     try {
       switch (type) {
         case "add-category":
-          const response = await dispatch(checkCategory(event.name)).unwrap();
-          if (response.message === "Category Already Exists.") {
-            setError("category", {
-              type: "duplicate",
-              message: "Category already exists",
-            });
-            setFormSubmitting(false);
-            return;
-          }
           dispatch(createNewCategory(event)).unwrap();
           MySwal.fire({
             title: `Successfully Created!`,
@@ -176,7 +190,7 @@ const AddEditCategory = ({
             </Col>
             <Col md={6} xs={12}>
               <Label className='form-label' htmlFor='status'>
-                Status
+                Status<span style={{ color: "red" }}> * </span>
               </Label>
               <Controller
                 control={control}
