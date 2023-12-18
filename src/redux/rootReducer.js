e7628898929289f1ddf2312dfe334f62994f6406
shubham.pangale <shubham.pangale@agami-tech.com>
2023-12-18 13:18:59 +0530

    Changes in rootReducer file

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,4 @@
 // ** Reducers Imports
-
-import navbar from './navbar'
 import layout from './layout'
 import auth from './authentication'
 import dashboard from '@src/views/dashboard/store'
@@ -10,13 +8,6 @@ import team from '@src/views/team/store'
 import task from '@src/views/task/store'
 import subcategory from '@src/views/subategory/store'
 import navbar from "./navbar";
-import layout from "./layout";
-import auth from "./authentication";
-import dashboard from "@src/views/dashboard/store";
-import permissions from "@src/views/permission/role-table/store";
-import users from "@src/views/users/store";
-import team from "@src/views/team/store";
-import task from "@src/views/task/store";
 import category from "@src/views/category/store";
 
 const rootReducer = {
